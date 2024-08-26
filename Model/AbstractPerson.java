a306b5f37454cb346a4e900f2b707d43c0cc4973
ArefOleg <serd-075@mail.ru>
2024-08-26 12:41:59 +0300

    AbstarctPerson

---
+++ Model/AbstractPerson.java
@@ -0,0 +1,11 @@
+package Model;
+
+public class AbstractPerson {
+    private String name;
+    private Integer heroMissionCompleteCount;
+    private Integer activity;
+    private Integer inventory;
+    private Integer hp;
+    private Integer implantSlote;
+    
+}
